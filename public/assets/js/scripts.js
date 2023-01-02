6a55740f9bd3624ae61b5c773cb6e9f9c07ee2e8
griffinjoshs <leveloutusa@gmail.com>
2023-01-02 11:39:42 -0500

    new ui!!!!!

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -1,19 +1,5 @@
-// TODO: make createimg function more intellegent for modal. the function needs to handle the image size so that it sometimes
-// can take up the whole container that its in. use JS to size the image accordingly.
-
 const webDevelopment = "webDev";
 const digitalMarketing = "digMark";
-let url = window.location.protocol + "//" + window.location.hostname;
-if (
-  window.location.port != "" ||
-  window.location.port != null ||
-  window.location.port != undefined
-) {
-  url += ":" + window.location.port;
-}
-console.log(url);
-
-let serviceBtn = document.querySelectorAll(".service-btn");
 
 // page animations
 const animate = (classes) => {
@@ -35,16 +21,21 @@ const animate = (classes) => {
 
 const appearOptions = {
   root: null,
-  threshold: 1,
+  threshold: 2,
   rootMargin: "0px",
+  // rootPadding: "-100px"
 };
 
-// pre-loader
-
-// call function onload
-{
-  /* <img src="./assets/images/sideflip-copy.gif"/> */
+let url = window.location.protocol + "//" + window.location.hostname;
+if (
+  window.location.port != "" ||
+  window.location.port != null ||
+  window.location.port != undefined
+) {
+  url += ":" + window.location.port;
 }
+console.log(url);
+
 const addImage = (imageContainerTag, src) => {
   let image = document.createElement("img");
   let imageContainer = document.querySelector(imageContainerTag);
@@ -55,7 +46,7 @@ const addImage = (imageContainerTag, src) => {
   console.log(imageContainer);
 };
 
-const preLoad = () => {
+window.addEventListener("load", () => {
   addImage("#loadpic", "/assets/images/grifflogobold.png");
   addImage("#loadgif", "/assets/images/sideflip-copy.gif");
   setTimeout(() => {
@@ -67,24 +58,29 @@ const preLoad = () => {
     setTimeout(() => {
       document.querySelector("#load-wrap").classList.add("hide");
     }, 1100);
-    // page animations from the intersection observer
-    document.querySelectorAll(".fade-in").forEach((fader) => {
-      animate(fader);
-    });
 
-    document.querySelectorAll(".slide-in").forEach((slider) => {
-      animate(slider);
-    });
+  document.querySelectorAll(".fade-in").forEach((fader) => {
+    animate(fader);
+  });
 
-    document.querySelectorAll(".roll-in").forEach((roll) => {
-      animate(roll);
-    });
+  document.querySelectorAll(".slide-in").forEach((slider) => {
+    animate(slider);
+  });
 
-    document.querySelectorAll(".color-change").forEach((change) => {
-      animate(change);
-    });
-  }, 1100);
-};
+  document.querySelectorAll(".roll-in").forEach((roll) => {
+    animate(roll);
+  });
+
+  document.querySelectorAll(".blueliquid2").forEach((change) => {
+    animate(change);
+  });
+
+  document.querySelectorAll(".color-flicker").forEach((change) => {
+    animate(change);
+  });
+}, 1100);
+});
+
 let loadBoxes = [];
 const loadBoxContainer = document.querySelector(".load-box-container");
 
@@ -94,383 +90,184 @@ console.log(loadBoxes);
 
 loadBoxContainer.innerHTML = loadBoxes;
 
-window.addEventListener("load", () => {
-  preLoad();
-  loadNavBar();
-  loadMenu();
-});
-
-// nav and menu animations
-const header = document.getElementById("header");
-const displaySkill = document.getElementById("displaySkill");
-const underSection = document.querySelector("#under-section");
-const navbar = document.getElementById("navbar");
-const navMenu = document.getElementById("menu");
-const clearForMenu = Array.from(document.querySelectorAll(".clear-for-menu"));
-const heroSection = document.querySelector(".hero-section");
-
-const hideElementsForAnimation = () => {
-  clearForMenu.forEach((clear) => {
-    clear.classList.add("hide");
-  });
-};
-
-const showElementsAfterAnimation = () => {
-  clearForMenu.forEach((clear) => {
-    clear.classList.remove("hide");
-  });
-};
-
-const showHero = () => {
-  if (heroSection != null) {
-    heroSection.setAttribute("style", "visibility: visible;");
-  }
-};
-
-const hideHero = () => {
-  if (heroSection != null) {
-    heroSection.setAttribute("style", "visibility: hidden;");
-  }
-};
-
-const addAnimation = () => {
-  document.body.classList.add("color-slide");
-};
-
-const hideMenu = () => {
-  if (!navMenu.classList.contains("hide")) {
-    navMenu.classList.add("hide");
-    navMenu.classList.remove("flex");
-  }
-};
-
-const showMenu = () => {
-  if (navMenu.classList.contains("hide")) {
-    navMenu.classList.remove("hide");
-    navMenu.classList.add("flex");
-  }
-};
-
-const linkTo = (link) => {
-  addAnimation();
-  hideMenu();
-  hideElementsForAnimation();
-  setTimeout(() => {
-    window.location.href = link;
-    showHero();
-    resetAnimation();
-  }, 2500);
-};
-
-const loadNavBar = () => {
-  navbar.innerHTML += `
-    <div class="nav-left slide-in left" onclick="linkTo('/')">
-              <img src="./assets/images/grifflogobold.svg" height="80" width="80" id='navbar-logo' style='margin-right: 5px;'>
-              <div class="name-logo">
-                  <h1>Griffin</h1>
-              </div>
-          </div>
-          <div class="nav-right slide-in right">
-         <button class='icon' id='hamburger' onclick='displayMenu()'>
-          <ion-icon name="menu-outline"></ion-icon>
-           </button>
-         <button class='icon hide' id='close' onclick='goBackToPage()'>
-         <ion-icon name="close-outline"></ion-icon>
-         </button>
-         <button class='icon hide' id='scrollTop'>
-         </button>
-         </div>
-    `;
-};
-
-const loadMenu = () => {
-  navMenu.classList.remove("flex");
-  navMenu.innerHTML += `
-    <div class='menu-btns'>
-      <button onclick="linkTo('/')">
-      <h5>Home</h5>
-      </button>
-      <button onclick="linkTo('/')">
-      <h5>Resume</h5>
-      </button>
-       <button onclick="linkTo('${socialMediaBtns[0].link}')">
-       <h5>${socialMediaBtns[0].icon} ${socialMediaBtns[0].name}</h5>
-       </button>
-       <button onclick="linkTo('${socialMediaBtns[1].link}')">
-       <h5>${socialMediaBtns[1].icon} ${socialMediaBtns[1].name}</h5>
-       </button>
-    </div>
-  
-    `;
-};
-
-const resetAnimation = () => {
-  document.body.classList.remove("color-slide");
-};
-
-const displayMenu = async () => {
-  addAnimation();
-  hideElementsForAnimation();
-  hideHero();
-  setTimeout(() => {
-    showMenu();
-    document.getElementById("hamburger").classList.add("hide");
-    document.getElementById("close").classList.remove("hide");
-    resetAnimation();
-  }, 2800);
-};
-
-const goBackToPage = () => {
-  hideElementsForAnimation();
-  hideMenu();
-  addAnimation();
-  setTimeout(() => {
-    showElementsAfterAnimation();
-    showHero();
-    document.getElementById("hamburger").classList.remove("hide");
-    document.getElementById("close").classList.add("hide");
-    resetAnimation();
-    underSection.classList.add("hide");
-  }, 2800);
-};
-
-// loadNavBar();
-// loadMenu();
-
-// content spawning
-
-const skills = (content) => {
-  content.map(
-    (skillData) =>
-      (document.querySelector(".box-section").innerHTML += `
-    <div class="info-box">
-  <h3 class="box-icon">${skillData.icon}</h3>
-  <div class="info-text">
-      <h4>${skillData.skill}</h4>
-      </div>
-  </div>
-</div>
-    `)
-  );
-  document.querySelectorAll(".info-box").forEach((info) => {
-    animate(info);
-  });
-};
-
-const contactSection = (title) => {
-  return `
-<div class="contact-area">
-<div class="contact-details">
-    <div class="addresses">
-    <div class="way-of-contact">
-    <a href = "mailto: griffin@toksecretformula.com">
-        <h4><ion-icon name="mail-outline"></ion-icon></h4>
-        <h5>griffin@toksecretformula.com</h5>
-    </div>
-    </a>
-    <a href="tel:+732-939-1309">
-    <div class="way-of-contact">
-      <h4><ion-icon name="phone-portrait-outline"></ion-icon></h4>
-      <h5>732-939-1309</h5>
-  </div>
-  </a>
-</div>
-<div class="divider"></div>
-<div class="social-btns">
-
-</div>
-<div class="divider"></div>
-<h4 class="footer-text">© 2022 Griffins Web Services.</h4>
-</div>
-<div class="contact-form">
-    <form action="/api/subscribe" method="POST">
-      <div class="form-name-section">
-          <input type="text" class='contact-input input-field name-input' name="firstName" placeholder="First Name">
-          <input type="text" class='contact-input input-field name-input' name="lastName" placeholder="Last Name">
-      </div>
-      <input type="email" class='contact-input input-field email' name="email" placeholder="Email Address">
-      <input type="text" class='contact-input input-field subject' name="subject" placeholder="Subject">
-      <textarea class='contact-input textarea' name="message" placeholder="Message"></textarea>
-      <button type="submit" class="form-submit square-btn transparent-btn">Send</button>
-    </form>
-</div>
-</div>
-<div class="verticle-title">
-  <h3 class="section-header">${title}</h3>
-</div>
-  `;
-};
-
-const contactPage = document.getElementById("contactPage");
-
 let setHTML = (query, data) => {
   document.querySelector(query).innerHTML = data;
 };
 
-const showSocials = (htmlElement) => {
-  socialMediaBtns.forEach((socialBtns) => {
-    htmlElement.innerHTML += `
-    <a href='${socialBtns.link}' target='_blank'>
-    <button>
-    ${socialBtns.icon}
-    </button>
-    </a>
-    `;
+// const mapContent = (content, selector) => {
+//     content.map(
+//       (data) =>
+//         (selector.innerHTML += `
+//     <div class="skill">
+//         <h4>${data.skill}</h4>
+//         </div>
+//       `)
+//     );
+//     document.querySelectorAll(".info-box").forEach((element) => {
+//       animate(element);
+//     });
+//   };
+
+const mapSkills = (content, selector) => {
+  selector.innerHTML = '',
+  content.map(
+    (data) =>
+      (
+        selector.innerHTML += `
+      <div class="skill color-flicker">
+          <h4>${data.skill}</h4>
+          </div>
+        `)
+  );
+  document.querySelectorAll(".skill").forEach((element) => {
+    animate(element);
   });
 };
 
-const isEven = (num) => {
-  return num % 2 == 0;
+const mapProfessions = (content, selector) => {
+  selector.innerHTML = '',
+  content.map(
+    (data) =>
+      (selector.innerHTML += `
+          <h4 class='slide-in'>${data.title}</h4>
+        `)
+  );
+  document.querySelectorAll('.things-i-can-do h4:nth-child(odd)').forEach(
+    thing => {
+      thing.classList.add('left')
+    }
+  )
+  document.querySelectorAll('.things-i-can-do h4:nth-child(even)').forEach(
+    thing => {
+      thing.classList.add('right')
+    }
+  )
+  document.querySelectorAll(".things-i-can-do h4").forEach((element) => {
+    animate(element);
+  });
 };
+
+const mapSpecialties = (content, selector) => {
+  selector.innerHTML = '',
+  content.map(
+    (data) =>
+      (selector.innerHTML += `
+       <div class="message-info-tab slide-in left">
+          <div class="message-info-text">
+            <h3>${data.name}</h3>
+            <h6>${data.explanation}</h6>
+          </div>
+          </div>
+        `)
+  );
+  document.querySelectorAll(".message-info-tab").forEach((element) => {
+    animate(element);
+  });
+};
+
+const displayProjects = (content, selector) => {
+  // document.write (content.join(''));
+  selector.innerHTML = '',
+  content.forEach(
+    (data) => 
+    (selector.innerHTML += `
+    <div class="project" style="background: url('${data.imageUrl}');background-position: center;background-repeat: no-repeat;background-size: cover;">
+    <div class="hover-project">
+      <div class="project-skills">
+      ${data.skills.map(
+          (skill) => (
+            `<div class="skill">${skill.skill}</div>`
+          )
+        )
+      }
+      </div>
+      <div class='project-btn-sec'>
+      <a href="${data.websiteLink}"></a><button><ion-icon name="open-outline"></ion-icon></button>
+      <a href="/"></a><button><ion-icon name="logo-github" class='social-icon'></ion-icon></button>
+    </div>
+    </div>
+  </div>  
+    `
+    )
+  )
+  document.querySelectorAll('.project:nth-child(odd)').forEach(
+    thing => {
+      thing.classList.add('left')
+    }
+  )
+  document.querySelectorAll('.project:nth-child(even)').forEach(
+    thing => {
+      thing.classList.add('right')
+    }
+  )
+  document.querySelectorAll(".project").forEach((element) => {
+    animate(element);
+  }
+  );
+}
 
 const pickService = (content) => {
-  // console.log(content);
-  if (underSection.classList.contains("hide")) {
-    underSection.classList.remove("hide");
-  }
-  (document.querySelector(".typeOfWork-section").innerHTML = ""),
-    (document.querySelector(".box-section").innerHTML = ""),
-    content.workOptions.map(
-      (work) =>
-        (document.querySelector(".typeOfWork-section").innerHTML += `
-    <div class="type-of-work slide-in left">
-    <h3>${work.optionIcon}</h3>
-    <h5>${work.optionName}</h5>
-    </div>
-    `)
-    );
-  document.querySelectorAll(".type-of-work").forEach((info) => {
-    animate(info);
-  });
-  skills(content.skills);
-
-  setHTML(".sub-header", content.subHeader);
-  setHTML(".skill-h", content.header);
-  setHTML(".about-me p", content.aboutParagraph);
-
-  fetch("/api/projects")
-    .then((response) => response.json())
-    .then((resData) => {
-      resData.results.forEach((data) => {
-        let projectData = [];
-        // console.log(data.topic);
-        console.log(data);
-        if (data.topic.includes(content.identifyer)) {
-          projectData.push(data);
-          // console.log(projectData);
-          let iterator = 1;
-          projectData.forEach((data) => {
-            let projectSkills = data.skills.map((s) => `<li>${s}</li>`).join('')
-            // console.log(data.skills)
-            console.log(projectSkills)
-            document.querySelector(".project-section").innerHTML += `
-    <div class="project-item">
-            <div class="project-img" style="background: url(${data.image});"> </div>
-            <div class='project-text'>
-            <h5>${data.projectName}</h5>
-            <ul class='project-skills'>${projectSkills}</ul>
-            <p>${data.projectDescription}</p>
-            <div class='project-btn-section'>
-            <button class="project-btn"><ion-icon name="download-outline"></ion-icon></button>
-            <button class='project-btn'><ion-icon name="logo-github"></ion-icon></button>
-            </div>
-            </div>
-    </div>
-    `;
-
-            iterator++;
-            console.log(iterator);
-          });
-        }
-      });
-    });
-
-  setHTML("#contact", contactSection(content.contactTitle));
-
-  const socialBtnSec = document.querySelector(".social-btns");
-
-  showSocials(socialBtnSec);
-
-  document.querySelector(".form-submit").addEventListener("click", (e) => {
-    e.preventDefault;
-  });
+  console.log(content);
+  setHTML(".image-bg-text h2", content.skillName);
+  setHTML(".text-box p", content.aboutParagraph);
+  setHTML(".headers h3", content.header);
+  setHTML(".headers h2", content.tagline);
+  // page animations from the intersection observer
+  mapSkills(
+    content.skills, 
+    document.querySelector(".skills")
+  );
+  mapProfessions(
+    content.professions,
+    document.querySelector(".things-i-can-do")
+  );
+  mapSpecialties(
+    content.mySpecialties,
+    document.querySelector(".message-info")
+  );
+  displayProjects(
+    content.projects,
+    document.querySelector('.my-projects')
+  )
 };
+
+let serviceBtn = document.querySelectorAll(".service-btn");
 
 // set specific skill's data to page
 const setPageData = (chosenSkill) => {
-  // console.log(chosenSkill);
-  // let chosenSkillData;
   skillData.forEach((skill) => {
-    if (chosenSkill === skill.identifyer)
-      // console.log(skill)
+    if (chosenSkill === skill.identifyer) {
+      // console.log(skill.identifyer)
       pickService(skill);
+    } else {
+      console.log("fuck my life");
+    }
   });
 };
 
-let chosenSkill;
-
-// find clicked button
 serviceBtn.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     [].forEach.call(serviceBtn, (btn) => {
       btn.classList.remove("selected");
     });
-
+    window.location.href = "#about-me";
+    document.querySelector('.under-section').classList.remove('hide')
     let data = e.target.closest(".hero-btn");
     if (data.id.includes(webDevelopment)) {
       chosenSkill = webDevelopment;
+      // console.log(chosenSkill)
     } else if (data.id.includes(digitalMarketing)) {
       chosenSkill = digitalMarketing;
     }
     btn.classList.add("selected");
     setPageData(chosenSkill);
     // modal.className = "";
-    setTimeout(() => {
-      window.location.href = "#under-section";
-      if (chosenSkill === webDevelopment){
-      document.querySelector("#cert-modal").classList.add('webdev-Bg')
-      } else if (chosenSkill === digitalMarketing) {
-        document.querySelector("#cert-modal").classList.add('digmark-Bg')
-      }
-      openCertBtn = document.querySelectorAll(".cert-btn");
-      // console.log(openCertBtn)
-    }, 400);
   });
 });
 
-const modalSection = () => {
-  document.querySelector("#cert-modal").innerHTML = `
-  <button class='close-modal-btn'>
-    <ion-icon name="close-outline"></ion-icon>  
-  </button>
-  `;
-};
+document.querySelector('.contact-form button').addEventListener('click', (e) => {
+  setTimeout(() => {
+    window.location.reload()
+  }, 1100);
+})
 
-modalSection();
-
-document.querySelectorAll(".cert-btn").forEach((btn) => {
-  btn.addEventListener("click", (e) => {
-    openCertModal();
-  });
-});
-
-const openCertModal = () => {
-  console.log(document.querySelector("#cert-modal"));
-  // console.log(e.target);
-  document.querySelector(".cert-modal-container").classList.remove("hide");
-  document.querySelector(".cert-modal-container").classList.add("flex");
-  // document.querySelector("#cert-modal").classList.remove('webDev-cert')
-  // document.querySelector("#cert-modal").classList.remove('digMark-cert')
-  if (chosenSkill === webDevelopment) {
-    document.querySelector("#cert-modal").classList.add("webDev-cert");
-    // console.log(chosenSkill);
-  } else if (chosenSkill === digitalMarketing) {
-    document.querySelector("#cert-modal").classList.add("digMark-cert");
-    // console.log(chosenSkill);
-  }
-};
-
-document.querySelector(".close-modal-btn").addEventListener("click", (e) => {
-  document.querySelector(".cert-modal-container").classList.add("hide");
-  document.querySelector(".cert-modal-container").classList.remove("flex");
-});
